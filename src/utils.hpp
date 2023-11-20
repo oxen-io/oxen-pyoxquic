bd1894f4dc4bfea148f4f07d4a6a8124e88a7f33
Jason Rhinelander <jason@imaginary.ca>
2023-11-20 16:58:08 -0400

    DRY bstring casters, add ustring casters as well
    The bodies of these two classes were identical, except for the bstring
    vs bstring_view type; this DRYs them out.
    
    Also changed the type reported to python to `bytes` so that generated
    doc signatures look right on the Python side.

--- src/utils.hpp
+++ src/utils.hpp
@@ -11,76 +11,58 @@ namespace py = pybind11;
 
 PYBIND11_MAKE_OPAQUE(std::shared_ptr<void>);
 
-namespace PYBIND11_NAMESPACE
-{
-    namespace detail
-    {
-        template <>
-        struct type_caster<oxen::quic::bstring>
-        {
-          public:
-            PYBIND11_TYPE_CASTER(oxen::quic::bstring, const_name("bstring"));
-
-            bool load(handle src, bool)
-            {
-                char* buf;
-                ssize_t len;
-
-                if (PyBytes_AsStringAndSize(src.ptr(), &buf, &len) != 0)
-                    throw error_already_set{};
-
-                value = oxen::quic::bstring{
-                        reinterpret_cast<std::byte*>(buf), static_cast<size_t>(len)};
-                return true;
-            }
-
-            static handle cast(
-                    oxen::quic::bstring src, return_value_policy /* policy */, handle /* parent */)
-            {
-                auto obj = reinterpret_steal<object>(PYBIND11_BYTES_FROM_STRING_AND_SIZE(
-                        reinterpret_cast<const char*>(src.data()), src.size()));
-                if (!obj)
-                {
-                    pybind11_fail("Could not allocate bytes object!");
-                }
-                return obj;
-            }
-        };
-
-        template <>
-        struct type_caster<oxen::quic::bstring_view>
-        {
-          public:
-            PYBIND11_TYPE_CASTER(oxen::quic::bstring_view, const_name("bstring_view"));
-
-            bool load(handle src, bool)
-            {
-                char* buf;
-                ssize_t len;
-
-                if (PyBytes_AsStringAndSize(src.ptr(), &buf, &len) != 0)
-                    throw error_already_set{};
-
-                value = oxen::quic::bstring_view{
-                        reinterpret_cast<std::byte*>(buf), static_cast<size_t>(len)};
-                return true;
-            }
-
-            static handle cast(
-                    oxen::quic::bstring_view src, return_value_policy /* policy */,
-                    handle /* parent */)
-            {
-                auto obj = reinterpret_steal<object>(PYBIND11_BYTES_FROM_STRING_AND_SIZE(
-                        reinterpret_cast<const char*>(src.data()), src.size()));
-                if (!obj)
-                {
-                    pybind11_fail("Could not allocate bytes object!");
-                }
-                return obj;
-            }
-        };
-    }  // namespace detail
-}  // namespace PYBIND11_NAMESPACE
-
 namespace oxen::quic
-{}
+{
+    template <typename StringT>
+    struct pybytes_caster
+    {
+        static_assert(
+                sizeof(StringT::value_type) == 1 && !std::is_same_v<StringT::value_type, char>,
+                "pybytes_caster requires a 1-byte, non-char type");
+
+      public:
+        PYBIND11_TYPE_CASTER(StringT, const_name("bytes"));
+
+        bool load(handle src, bool)
+        {
+            char* buf;
+            ssize_t len;
+
+            if (PyBytes_AsStringAndSize(src.ptr(), &buf, &len) != 0)
+                throw error_already_set{};
+
+            value = StringT{
+                    reinterpret_cast<typename StringT::value_type*>(buf), static_cast<size_t>(len)};
+            return true;
+        }
+
+        static handle cast(StringT src, return_value_policy /* policy */, handle /* parent */)
+        {
+            auto obj = reinterpret_steal<object>(PYBIND11_BYTES_FROM_STRING_AND_SIZE(
+                    reinterpret_cast<const char*>(src.data()), src.size()));
+            if (!obj)
+            {
+                pybind11_fail("Could not allocate bytes object!");
+            }
+            return obj;
+        }
+    };
+}  // namespace oxen::quic
+
+namespace pybind11::detail
+{
+    template <>
+    struct type_caster<oxen::quic::bstring> : oxen::quic::pybytes_caster<oxen::quic::bstring>
+    {};
+    template <>
+    struct type_caster<oxen::quic::bstring_view>
+            : oxen::quic::pybytes_caster<oxen::quic::bstring_view>
+    {};
+    template <>
+    struct type_caster<oxen::quic::ustring> : oxen::quic::pybytes_caster<oxen::quic::ustring>
+    {};
+    template <>
+    struct type_caster<oxen::quic::ustring_view>
+            : oxen::quic::pybytes_caster<oxen::quic::ustring_view>
+    {};
+}  // namespace pybind11::detail
